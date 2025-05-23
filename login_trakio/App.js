0628aa35e7f7fdb2cc43d934a75608a08f81c62c
Android Studio <devandroid@ort>
2025-05-23 09:24:19 -0300

    avanzes de front end, logo y inputs para ingresar datos

--- login_trakio/App.js
+++ login_trakio/App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TextInput, ImageBackground, TouchableOpacity, Pressable, Image, Button } from 'react-native';
+import React, { useState } from 'react';
+
 
 export default function App() {
+  const imgLocal2 = require('./assets/logo.png');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='light'></StatusBar>
+      <Image source={imgLocal2} style={styles.logo} />
+      <TextInput style={styles.texto} placeholder='🤷    Salvador Soncini' />
+      <TextInput style={styles.gmail} placeholder='salvadorsoncini@gmail.com' />
+      <TextInput style={styles.contrsaena} placeholder='**********' />
+
+            <Button style={styles.boton} >
+                <Text style={styles.textoBoton}>Soy un infoPibe</Text>
+            </Button>
+
     </View>
   );
 }
@@ -13,8 +24,47 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1C1D1D',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  logo:
+  {
+    width:150,
+    height:150,
+    borderRadius:10,
+    marginTop:150,
+  },
+  texto: {
+    backgroundColor: '#fff',
+    marginTop: 80,
+    padding: 10,
+    paddingRight: 50,
+    paddingLeft: 50,
+    borderRadius: 7,
+    borderWidth: 2.5,        
+    borderColor: '#1359BF',   
+  },  
+  gmail:
+  {
+    backgroundColor: '#fff',
+    padding:10,
+    paddingRight:50,
+    paddingLeft:50,
+    borderRadius:7,
+    marginTop:30,
+    borderWidth: 2.5,        
+    borderColor: '#1359BF',
+  },
+  contrsaena:
+  {
+    backgroundColor: '#fff',
+    padding:10,
+    paddingRight:50,
+    paddingLeft:50,
+    borderRadius:7,
+    marginTop:30,
+    textAlign:'center',
+    borderWidth: 2.5,        
+    borderColor: '#1359BF',
   },
 });
